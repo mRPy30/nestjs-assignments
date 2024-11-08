763a3559aa6e12d1648b1cb00ee322b8acc4486e
mRPy30 <janviererickson.araque@cvsu.edu.ph>
2024-11-08 15:17:12 +0800

    Completed Assignment 2: Prime Number

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -19,4 +19,21 @@ export class AssignmentsController {
     }
     return sequence;
   }
+
+  @Get('prime/:number')
+  checkPrime(@Param('number') number: number): { isPrime: boolean } {
+    const isPrime = this.isPrime(Number(number));
+    return { isPrime };
+  }
+
+  private isPrime(num: number): boolean {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
 }
